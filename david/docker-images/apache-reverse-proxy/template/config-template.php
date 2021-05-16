6f0bb8d044d4f003cd686a59c7375f2fa8e85436
David González León <david.gonzalezleon@heig-vd.ch>
2021-05-16 15:08:51 +0200

    debut load balancer

--- david/docker-images/apache-reverse-proxy/template/config-template.php
+++ david/docker-images/apache-reverse-proxy/template/config-template.php
@@ -1,14 +1,26 @@
 <?php
-   $static_app = getenv('STATIC_APP');
-   $dynamic_app = getenv('DYNAMIC_APP');
+   $static_app1 = getenv('STATIC_APP1');
+   $static_app2 = getenv('STATIC_APP2');
+   $dynamic_app1 = getenv('DYNAMIC_APP1');
+   $dynamic_app2 = getenv('DYNAMIC_APP2');
 ?>
 
 <VirtualHost *:80>
          ServerName demo.res.ch
 
-         ProxyPass '/api/animals/' 'http://<?php print "$dynamic_app"?>/'
-         ProxyPassReverse '/api/animals/' 'http://<?php print "$dynamic_app"?>/'
+         <Proxy 'balancer://dynamic'>
+            BalancerMember 'http://<?php print "$dynamic_app1"?>/'
+            BalancerMember 'http://<?php print "$dynamic_app2"?>/'
+         </Proxy>
 
-         ProxyPass '/' 'http://<?php print "$static_app"?>/'
-         ProxyPassReverse '/' 'http://<?php print "$static_app"?>/'
+         ProxyPass '/api/animals/' 'balancer://dynamic'
+         ProxyPassReverse '/api/animals/' 'balancer://dynamic'
+
+         <Proxy 'balancer://static'>
+            BalancerMember 'http://<?php print "$static_app1"?>/'
+            BalancerMember 'http://<?php print "$static_app2"?>/'
+         </Proxy>
+
+         ProxyPass '/' 'balancer://static'
+         ProxyPassReverse '/' 'balancer://static'
 </VirtualHost>
