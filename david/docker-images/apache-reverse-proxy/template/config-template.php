5bfef4c05b606c827385a307f9e2ef80c8c0e977
David González León <david.gonzalezleon@heig-vd.ch>
2021-05-17 10:05:35 +0200

    added sticky balancing

--- david/docker-images/apache-reverse-proxy/template/config-template.php
+++ david/docker-images/apache-reverse-proxy/template/config-template.php
@@ -8,19 +8,23 @@
 <VirtualHost *:80>
          ServerName demo.res.ch
 
+         Header add Set-Cookie "ROUTEID=.%{BALANCER_WORKER_ROUTE}e; path=/" env=BALANCER_ROUTE_CHANGED
+
          <Proxy 'balancer://dynamic'>
-            BalancerMember 'http://<?php print "$dynamic_app1"?>/'
-            BalancerMember 'http://<?php print "$dynamic_app2"?>/'
+            BalancerMember 'http://<?php print "$dynamic_app1"?>/' route=1
+            BalancerMember 'http://<?php print "$dynamic_app2"?>/' route=2
+            ProxySet stickysession=ROUTEID
          </Proxy>
 
-         ProxyPass '/api/animals/' 'balancer://dynamic'
-         ProxyPassReverse '/api/animals/' 'balancer://dynamic'
+         ProxyPass '/api/animals/' 'balancer://dynamic/'
+         ProxyPassReverse '/api/animals/' 'balancer://dynamic/'
 
          <Proxy 'balancer://static'>
-            BalancerMember 'http://<?php print "$static_app1"?>/'
-            BalancerMember 'http://<?php print "$static_app2"?>/'
+            BalancerMember 'http://<?php print "$static_app1"?>/' route=1
+            BalancerMember 'http://<?php print "$static_app2"?>/' route=2
+            ProxySet stickysession=ROUTEID
          </Proxy>
 
-         ProxyPass '/' 'balancer://static'
-         ProxyPassReverse '/' 'balancer://static'
+         ProxyPass '/' 'balancer://static/'
+         ProxyPassReverse '/' 'balancer://static/'
 </VirtualHost>
