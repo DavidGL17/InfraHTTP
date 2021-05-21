28a6d83b9a35eaa057224cf65e612f4362495373
David González León <david.gonzalezleon@heig-vd.ch>
2021-05-21 14:11:55 +0200

    fix load balancing

--- david/docker-images/apache-reverse-proxy/template/config-template.php
+++ david/docker-images/apache-reverse-proxy/template/config-template.php
@@ -5,26 +5,26 @@
    $dynamic_app2 = getenv('DYNAMIC_APP2');
 ?>
 
+<Proxy 'balancer://dynamic'>
+   BalancerMember 'http://<?php print "$dynamic_app1"?>' route=1
+   BalancerMember 'http://<?php print "$dynamic_app2"?>' route=2
+   ProxySet stickysession=ROUTEID
+</Proxy>
+
+<Proxy 'balancer://static'>
+   BalancerMember 'http://<?php print "$static_app1"?>' route=1
+   BalancerMember 'http://<?php print "$static_app2"?>' route=2
+   ProxySet stickysession=ROUTEID
+</Proxy>
+
 <VirtualHost *:80>
          ServerName demo.res.ch
 
          Header add Set-Cookie "ROUTEID=.%{BALANCER_WORKER_ROUTE}e; path=/" env=BALANCER_ROUTE_CHANGED
 
-         <Proxy 'balancer://dynamic'>
-            BalancerMember 'http://<?php print "$dynamic_app1"?>/' route=1
-            BalancerMember 'http://<?php print "$dynamic_app2"?>/' route=2
-            ProxySet stickysession=ROUTEID
-         </Proxy>
-
          ProxyPass '/api/animals/' 'balancer://dynamic/'
          ProxyPassReverse '/api/animals/' 'balancer://dynamic/'
-
-         <Proxy 'balancer://static'>
-            BalancerMember 'http://<?php print "$static_app1"?>/' route=1
-            BalancerMember 'http://<?php print "$static_app2"?>/' route=2
-            ProxySet stickysession=ROUTEID
-         </Proxy>
-
+         
          ProxyPass '/' 'balancer://static/'
          ProxyPassReverse '/' 'balancer://static/'
 </VirtualHost>
